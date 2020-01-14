1d9c427873b6b2d332690a1ac17bc98b872bb880
thanhduong126 <hoangthanhduong09021995@gmail.com>
2020-01-14 16:59:29 +0700

    component-hopthu

--- hen-ho/src/app/app-routing.module.ts
+++ hen-ho/src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { GuithuComponent } from './client/HopThu-Component/guithu/guithu.component';
-import { XemthuComponent } from './client/HopThu-Component/xemthu/xemthu.component';
-import { QuanlyhopthuComponent } from './client/HopThu-Component/quanlyhopthu/quanlyhopthu.component';
-
 
 const routes: Routes = [
-
 ];
 
 @NgModule({
