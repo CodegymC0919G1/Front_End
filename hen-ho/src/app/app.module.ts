ff61fde89f5f82b2f9d5c0e3119302d57ab5e89f
hoang-123 <minhhoang120294@gmail.com>
2020-01-14 15:58:44 +0700

    hoang

--- hen-ho/src/app/app.module.ts
+++ hen-ho/src/app/app.module.ts
@@ -6,12 +6,20 @@ import { ShareModule} from 'src/app/share/share.module';
 import { HeaderComponent } from './client/layout/header/header.component';
 import { FooterComponent } from './client/layout/footer/footer.component';
 import { SlideComponent } from './client/layout/slide/slide.component';
+import { DangNhapComponent } from './client/login/dang-nhap/dang-nhap.component';
+import { TopComponent } from './client/top100/top/top.component';
+import { DangThongBaoComponent } from './quantrivien/thong-bao/dang-thong-bao/dang-thong-bao.component';
+import { LoginAdminComponent } from './quantrivien/login-admin/login-admin/login-admin.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    SlideComponent
+    SlideComponent,
+    DangNhapComponent,
+    TopComponent,
+    DangThongBaoComponent,
+    LoginAdminComponent
   ],
   imports: [
     BrowserModule,
---
+++ hen-ho/src/app/quantrivien/thong-bao/dang-thong-bao/dang-thong-bao.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dang-thong-bao',
+  templateUrl: './dang-thong-bao.component.html',
+  styleUrls: ['./dang-thong-bao.component.scss']
+})
+export class DangThongBaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
