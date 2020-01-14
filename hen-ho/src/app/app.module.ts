8ee374fc79f8f4e4075266c24eba671bc4939c30
https://github.com/trungnam95/C0919G1_Module2_NguyenTrungNam.git <nguyentrungnam100495@gmail.com>
2020-01-14 16:51:52 +0700

    work

--- hen-ho/src/app/app.module.ts
+++ hen-ho/src/app/app.module.ts
@@ -6,12 +6,18 @@ import { ShareModule} from 'src/app/share/share.module';
 import { HeaderComponent } from './client/layout/header/header.component';
 import { FooterComponent } from './client/layout/footer/footer.component';
 import { SlideComponent } from './client/layout/slide/slide.component';
+import { DanhSachHoiNhomComponent } from './quantrivien/quanlihoinhom/danh-sach-hoi-nhom/danh-sach-hoi-nhom.component';
+import { ChiTietHoiNhomComponent } from './quantrivien/quanlihoinhom/chi-tiet-hoi-nhom/chi-tiet-hoi-nhom.component';
+import { ThongTinCuaNhomComponent } from './quantrivien/quanlihoinhom/thong-tin-cua-nhom/thong-tin-cua-nhom.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    SlideComponent
+    SlideComponent,
+    DanhSachHoiNhomComponent,
+    ChiTietHoiNhomComponent,
+    ThongTinCuaNhomComponent,
   ],
   imports: [
     BrowserModule,
--- hen-ho/src/app/quantrivien/quanlihoinhom/thong-tin-cua-nhom/thong-tin-cua-nhom.component.spec.ts
+++ hen-ho/src/app/quantrivien/quanlihoinhom/thong-tin-cua-nhom/thong-tin-cua-nhom.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HeaderComponent } from './header.component';
+import { ThongTinCuaNhomComponent } from './thong-tin-cua-nhom.component';
 
-describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+describe('ThongTinCuaNhomComponent', () => {
+  let component: ThongTinCuaNhomComponent;
+  let fixture: ComponentFixture<ThongTinCuaNhomComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ ThongTinCuaNhomComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HeaderComponent);
+    fixture = TestBed.createComponent(ThongTinCuaNhomComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
