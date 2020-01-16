4fa1adaaed0520b83d1288c3cfff5f11ec91e0bd
https://github.com/trungnam95/C0919G1_Module2_NguyenTrungNam.git <nguyentrungnam100495@gmail.com>
2020-01-16 09:56:09 +0700

    up

--- hen-ho/src/app/app-routing.module.ts
+++ hen-ho/src/app/app-routing.module.ts
@@ -1,18 +1,7 @@
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DangNhapComponent } from './client/login/dang-nhap/dang-nhap.component';
-import { TopComponent } from './client/top100/top/top.component';
-import { DangThongBaoComponent } from './quantrivien/thong-bao/dang-thong-bao/dang-thong-bao.component';
-
-<<<<<<< HEAD
-
 
 const routes: Routes = [];
-=======
-const routes: Routes = [
-];
->>>>>>> 4c636767d660bfbc41215940533e3bc7e18459b6
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
