57bbf3de3c6d75363d0e54632355a3826d92db52
hoang-123 <minhhoang120294@gmail.com>
2020-01-16 08:16:21 +0700

    hoang

--- hen-ho/src/app/app-routing.module.ts
+++ hen-ho/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DangNhapComponent } from './client/login/dang-nhap/dang-nhap.component';
+import { TopComponent } from './client/top100/top/top.component';
+import { DangThongBaoComponent } from './quantrivien/thong-bao/dang-thong-bao/dang-thong-bao.component';
 
 
 
-const routes: Routes = [
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
