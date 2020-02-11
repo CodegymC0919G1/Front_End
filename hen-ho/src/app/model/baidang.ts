329dfed8a8ee3e2d4b4fff0014f9bf512df3e185
BOC1096 <55128640+BOC1096@users.noreply.github.com>
2020-02-11 19:52:26 +0700

    Revert "Nam"

--- hen-ho/src/app/model/baidang.ts
+++ hen-ho/src/app/model/baidang.ts
@@ -1,24 +1,11 @@
-import { HoiNhom } from './hoinhom/hoinhom';
-import { QlThanhVien } from 'src/app/model/qlThanhVien';
-
 export class Baidang {
   idBaiDang: number;
+  idThanhVien: number;
   tieuDe: string;
   anh: string;
   noiDung: string;
-  xoa: boolean;
-  thoiGianDang: Date;
-  thanhVienDang: QlThanhVien;
-  idHoiNhom: HoiNhom;
-  // contructor( idBaiDang,tieuDe,anh,noiDung,xoa,thoiGianDang,thanhVienDang,idHoiNhom) {
-  //     this.idBaiDang=idBaiDang;
-  //     this.tieuDe=tieuDe;
-  //     this.anh=anh;
-  //     this.noiDung=noiDung;
-  //     this.xoa=xoa;
-  //     this.thoiGianDang=thoiGianDang;
-  //     this.thanhVienDang=thanhVienDang;
-  //     this.idHoiNhom=idHoiNhom;
-  // }
+  trangThai: string;
+  ngayDang: Date;
+  contructor() {}
 }
 
--- hen-ho/src/app/quantrivien/quanlihoinhom/danh-sach-hoi-nhom/danh-sach-hoi-nhom.component.ts
+++ hen-ho/src/app/quantrivien/quanlihoinhom/danh-sach-hoi-nhom/danh-sach-hoi-nhom.component.ts
@@ -1,19 +1,15 @@
-import { HoiNhomService } from "./../../../service/hoinhom/hoinhom.service";
-import { HoiNhom } from "../../../model/hoinhom/hoinhom";
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+
 @Component({
-  selector: "app-danh-sach-hoi-nhom",
-  templateUrl: "./danh-sach-hoi-nhom.component.html",
-  styleUrls: ["./danh-sach-hoi-nhom.component.scss"]
+  selector: 'app-danh-sach-hoi-nhom',
+  templateUrl: './danh-sach-hoi-nhom.component.html',
+  styleUrls: ['./danh-sach-hoi-nhom.component.scss']
 })
 export class DanhSachHoiNhomComponent implements OnInit {
-  hoiNhom: HoiNhom;
-  hoiNhomList: HoiNhom[] = [];
-  constructor(private hoiNhomService: HoiNhomService) {}
-  p = 1;
+
+  constructor() { }
+
   ngOnInit() {
-    this.hoiNhomService.getAllHoiNhom().subscribe(data => {
-      this.hoiNhomList = data;
-    });
   }
+
 }
